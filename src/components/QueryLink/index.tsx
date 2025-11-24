1130702626a5c41da25ce0b5d76f866c306424af
Matheus Marinho de Espíndula <1774@ucdb.br>
2025-11-24 11:00:56 -0400

    custom behavior

---
+++ src/components/QueryLink/index.tsx
@@ -0,0 +1,8 @@
+import { NavLink, useLocation } from "react-router-dom";
+
+export default function QueryLink({to, ...props} :any){
+    let location = useLocation();
+    return(
+        <NavLink to={to + location.search} {...props} />
+    );
+}
